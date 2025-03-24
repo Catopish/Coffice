e60bbdf0bbbf2cd35e8943a9eb3d4c1b699b48cf
angelcheyla <angelcheyla27@gmail.com>
2025-03-24 11:36:52 +0700

    zstack mapview health summary

--- Coffice/MapView.swift
+++ Coffice/MapView.swift
@@ -7,27 +7,56 @@
 
 import SwiftUI
 import MapKit
-
+import HealthKit
 
 struct MapView: View {
     var coordinate: CLLocationCoordinate2D
-
-
+    
+    
     var body: some View {
-        Map(position: .constant(.region(region)))
+        ZStack {
+            Map(position: .constant(.region(region)))
+            
+            Spacer()
+            VStack {
+                Spacer()
+                HStack {
+                    ZStack{
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 350, height: 150)
+                            .cornerRadius(10)
+                        VStack (alignment: .leading) {
+                            Text ("Your Activity")
+                            Text ("Move")
+                            Text ("258 kcal")
+                        }
+                    }
+                }
+                Spacer()
+                    .frame(height: 10)
+            }
+        }
     }
-
-
+    
+    
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008)
         )
-    }
+        
+                
+            }
 }
 
 
 
+
+   
+
+
+
 #Preview {
-    MapView(coordinate: CLLocationCoordinate2D(latitude: -6.3015152, longitude: 106.6551521))
+    MapView(coordinate: CLLocationCoordinate2D(latitude: -6.3019094, longitude: 106.6517333))
 }
