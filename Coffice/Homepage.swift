38f94086bf6cce5e44e4da2f3398fccd5d89f2b4
angelcheyla <angelcheyla27@gmail.com>
2025-04-07 09:00:26 +0700

    homepage update

--- Coffice/Homepage.swift
+++ Coffice/Homepage.swift
@@ -20,7 +20,6 @@ struct CoffeeShopStruct: Identifiable {
     
 }
 
-//Contoh buat max range
 @Observable
 class filterModel: ObservableObject {
     var maxRange: Double
@@ -43,13 +42,14 @@ struct Homepage: View {
     
     
     let coffeeShop : [CoffeeShopStruct] = [
-        CoffeeShopStruct(name: "Starbucks",location: "Lorem Ipsum", description: "lorem",distance: 127,steps: 123,calories: 123),
-        CoffeeShopStruct(name: "Fore",location: "Lorem Ipsum", description: "lorem",distance: 45 ,steps: 54,calories: 134),
-        CoffeeShopStruct(name: "Tamper",location: "Lorem Ipsum", description: "lorem",distance: 431,steps: 887,calories: 1223),
-        CoffeeShopStruct(name: "Kopi Kenangan",location: "Lorem Ipsum", description: "lorem",distance: 134,steps: 412,calories: 531),
-        CoffeeShopStruct(name: "Dunkin Donuts",location: "Lorem Ipsum", description: "lorem",distance: 486,steps: 212,calories: 431),
-        CoffeeShopStruct(name: "Kenangan Signature",location: "Lorem Ipsum", description: "lorem",distance: 325,steps: 78,calories: 431),
-        CoffeeShopStruct(name: "Tabemori",location: "Lorem Ipsum", description: "lorem",distance: 256,steps: 102,calories: 45)
+        CoffeeShopStruct(name: "Starbucks",location: "Lorem Ipsum", description: "The Breeze BSD",distance: 127,steps: 123,calories: 123),
+        CoffeeShopStruct(name: "Fore",location: "The Breeze BSD", description: "lorem",distance: 45 ,steps: 54,calories: 134),
+        CoffeeShopStruct(name: "Tamper",location: "The Breeze BSD", description: "lorem",distance: 431,steps: 887,calories: 1223),
+        CoffeeShopStruct(name: "% Arabica",location: "The Breeze BSD", description: "lorem",distance: 134,steps: 412,calories: 531),
+        CoffeeShopStruct(name: "Kenangan Signature",location: "The Breeze BSD", description: "lorem",distance: 325,steps: 78,calories: 431),
+        CoffeeShopStruct(name: "Tabemori",location: "Green Office Park 6", description: "lorem",distance: 256,steps: 102,calories: 45),
+        CoffeeShopStruct(name: "Lawson",location: "Green Office Park 6", description: "lorem",distance: 486,steps: 212,calories: 431),
+        CoffeeShopStruct(name: "36 Grams",location: "Green Office Park 1", description: "lorem",distance: 256,steps: 102,calories: 45)
     ]
     
     var filteredCoffeeshop: [CoffeeShopStruct] {
@@ -66,11 +66,14 @@ struct Homepage: View {
     var body: some View {
         VStack(alignment: .leading){
             userProfile()
-//            healthSummary(isLoading: $isLoading)
-//                .frame(height: 150)
             HealthDashboardView(isLoading: $isLoading)
+            Text("Where’s your coffee taking you today?")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .padding()
+                
             NavigationStack{
-                VStack{
+                VStack() {
                     List(filteredCoffeeshop.sorted(by: {$0.distance < $1.distance})) { shop in
                         Button(action: {
                             selectedCoffeeshop = shop
@@ -80,18 +83,23 @@ struct Homepage: View {
                                 Text(shop.name)
                                     .foregroundColor(.primary)
                                 Spacer()
-                                Text("\(Int(shop.distance)) M")
+                                Text("\(Int(shop.distance)) m")
                                     .foregroundColor(.primary)
+                                
                             }
-                            
                         }
                         .listRowSeparator(.hidden)
-                        
                     }
-                    //                    .navigationTitle("Coffee Shops")
+                    
+                    .background(Color.white)
+                    .scrollContentBackground(.hidden)
+                    .shadow(radius: 4)
+//                    .navigationTitle("Coffee Shops")
                     .searchable(text: $searchContent,placement: .navigationBarDrawer(displayMode: .always))
+                    .padding(.top, -30)
                 }
             }
+            .padding(.top, -20)
         }.overlay(
             coffeeshopInformation(showDetail: $showDetail, selectedCoffeeshop: $selectedCoffeeshop)
                 .animation(.easeInOut, value: showDetail)
@@ -107,59 +115,27 @@ struct Homepage: View {
     }
 }
 
-
-//struct userProfile: View {
+//struct ContentView: View {
+//    @AppStorage("userName") var userName: String = ""
+//    
 //    var body: some View {
-//        VStack {
-//            HStack {
-//                Image("avatar")
-//                    .resizable()
-//                    .frame(width: 50, height: 50)
-//                    .clipShape(Circle())
-//                    .padding()
-//
-//                VStack (alignment: .leading) {
-//                    Text("User's Name")
-//                    Text("User's Location")
-//                        .foregroundColor(.secondary)
-//
-//                }
-//                Spacer()
-//
-//                HStack{
-//                    Text ("[X] Streak")
-//                    Image(systemName: "flame.fill")
-//                }
-//                .padding()
-//                .background(Color.brown2)
-//                .foregroundColor(.white)
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .frame(width: 150, height: 20)
-//                .padding(.horizontal)
-
-
-struct ContentView: View {
-    @AppStorage("userName") var userName: String = ""
-    
-    var body: some View {
-        if userName.isEmpty {
-            OnboardingView()
-        } else {
-            Homepage()
-        }
-    }
-}
+//        if userName.isEmpty {
+//            OnboardingView()
+//        } else {
+//            Homepage()
+//        }
+//    }
+//}
 
 struct userProfile: View {
     @AppStorage("userName") var userName: String = ""
+    @StateObject var streakManager = StreakManager()
     var body: some View {
         HStack {
             VStack (alignment: .leading) {
                 Text("Hi, \(userName)!")
                     .font(.title)
                     .fontWeight(.semibold)
-                //                    .padding(.vertical)
-                
                 Text("Let’s walk and sip! ☕️")
                     .font(.subheadline)
                     .foregroundColor(.secondary)
@@ -169,7 +145,7 @@ struct userProfile: View {
             Spacer()
             
             HStack{
-                Text ("[X] Streak")
+                Text ("\(streakManager.streak) Streak")
                 Image(systemName: "flame.fill")
             }
             
@@ -178,7 +154,7 @@ struct userProfile: View {
             .foregroundColor(.white)
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .frame(width: 150, height: 20)
-            .padding(.horizontal)
+            .padding(.horizontal, 5)
         }
     }
 }
