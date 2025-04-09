8a5e535637c1f7793908414699c4c7f6e64a1fb0
Al <athila2001@gmail.com>
2025-04-09 18:55:08 +0700

    ganti %arabica dummy location

--- Coffice/Homepage.swift
+++ Coffice/Homepage.swift
@@ -62,7 +62,7 @@ struct Homepage: View {
         CoffeeShopStruct(name: "Fore", location: "The Breeze", description: "lorem", distance: 45, steps: 54, calories: 134, latitude: -6.302514, longitude: 106.654299, logo: "fore"),
         CoffeeShopStruct(name: "36 Grams", location: "Lorem Ipsum", description: "lorem", distance: 45, steps: 54, calories: 134, latitude: -6.301446, longitude: 106.650023, logo: "36grams"),
         CoffeeShopStruct(name: "Tamper", location: "The Breeze", description: "lorem", distance: 431, steps: 887, calories: 1223, latitude: -6.301870, longitude: 106.654210, logo: "tamper"),
-        CoffeeShopStruct(name: "% Arabica", location: "The Breeze", description: "lorem", distance: 431, steps: 887, calories: 1223, latitude: -6.2912875957704, longitude: 106.6275295526279, logo: "arabica"),
+        CoffeeShopStruct(name: "% Arabica", location: "Lorem Ipsum", description: "lorem", distance: 431, steps: 887, calories: 1223, latitude: -6.30179, longitude: 106.65321, logo: "arabica"),
         CoffeeShopStruct(name: "Kenangan Signature", location: "The Breeze", description: "lorem", distance: 134, steps: 412, calories: 531, latitude: -6.301535, longitude: 106.653458, logo: "kenangan"),
         CoffeeShopStruct(name: "Tabemori", location: "GOP 1", description: "lorem", distance: 256, steps: 102, calories: 45, latitude: -6.302768, longitude: 106.653470, logo: "tabemori"),
         CoffeeShopStruct(name: "Dummy", location: "Lorem Ipsum", description: "lorem", distance: 0, steps: 123, calories: 123, latitude: -6.302141, longitude: 106.652327, logo: "sbux"),
