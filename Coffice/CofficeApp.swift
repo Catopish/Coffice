5c91057b10b5de33f86b32a5faa6d7ca50512058
angelcheyla <angelcheyla27@gmail.com>
2025-04-14 11:24:20 +0700

    edit splash screen

--- Coffice/CofficeApp.swift
+++ Coffice/CofficeApp.swift
@@ -10,15 +10,29 @@ import SwiftData
 
 @main
 struct CofficeApp: App {
+    @State private var isSplashActive = true
     @AppStorage("userName") var userName: String = ""
     
     var body: some Scene {
         WindowGroup {
-            if userName.isEmpty {
-                OnboardingView()
-            }
-            else {
-                Homepage()
+            if isSplashActive {
+                SplashScreenView()
+                    .onAppear {
+                        // Delay 2 detik sebelum masuk ke main view
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            withAnimation {
+                                isSplashActive = false
+                            }
+                        }
+                    }
+            } else {
+                
+                if userName.isEmpty {
+                    OnboardingView()
+                }
+                else {
+                    Homepage()
+                }
             }
         }
     }
